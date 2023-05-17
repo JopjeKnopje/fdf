f4d13feff23986106edab2f42335fb4513a621e9
JopjeKnopje <jboeve009@gmail.com>
2023-05-17 17:23:41 +0200

    atoi_hex not working yet

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: jboeve <marvin@42.fr>                       +#+                      */
 /*                                                  +#+                       */
 /*   Created: 2023/05/15 11:33:08 by jboeve        #+#    #+#                 */
-/*   Updated: 2023/05/17 17:16:21 by joppe         ########   odam.nl         */
+/*   Updated: 2023/05/17 17:21:49 by joppe         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@
 
 int32_t	main(int32_t argc, const char *argv[])
 {
-	const char *s = "0xff123";
-	uint32_t a = strtol(s, NULL, 0);
+	const char *s = "ff123";
+	uint32_t a = strtol(s, NULL, 16);
 	uint32_t b = ft_atoi_hex(s);
 	printf("a %d\n", a);
 	printf("b %d\n", b);
--- src/utils.c
+++ src/utils.c
@@ -6,15 +6,40 @@
 /*   By: joppe <jboeve@student.codam.nl>              +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/05/17 17:11:15 by joppe         #+#    #+#                 */
-/*   Updated: 2023/05/17 17:16:45 by joppe         ########   odam.nl         */
+/*   Updated: 2023/05/17 17:23:08 by joppe         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdint.h>
-
+#include "fdf.h"
 
 uint32_t ft_atoi_hex(const char *s)
 {
+	int	i;
+	int	num;
+	int	sign;
 
-	return (0);
+	num = 0;
+	sign = 0;
+	i = 0;
+	while (s[i] == ' ' || (s[i] >= 9 && s[i] <= 13))
+		i++;
+	while ((s[i] == '-' || s[i] == '+') && !sign)
+	{
+		if (s[i] == '-')
+			sign = -1;
+		if (s[i] == '+')
+			sign = 1;
+		i++;
+	}
+	if (!sign)
+		sign = 1;
+	while (s[i])
+	{
+		if (ft_isdigit(s[i]))
+			num = num * 16 + s[i] - '0';
+		else if (s[i] >= 'a' && s[i] <= 'f')
+			num = num * 16 + s[i] - ('x' - 'a');
+		i++;
+	}
+	return (num * sign);
 }
